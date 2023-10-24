d402937c9e7e2c67eecd4a08f2eea10f64a87b19
ahmtbr <68566330+ahmtbr@users.noreply.github.com>
2023-10-24 17:37:41 +0300

    HW2

---
+++ ass2/redbox.js
@@ -0,0 +1,9 @@
+// [TO-DO] Please change the color to blue
+// CS405 FALL 23/24 Ahmet Burak Ekmekcioglu
+const fragmentShaderSource = `
+precision mediump float;
+
+void main() {
+  gl_FragColor = vec4(0.0, 0.0, 1.0, 1.0);
+}
+`;
